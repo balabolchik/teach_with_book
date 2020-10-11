9e083e07de2eeb0998c951470cd0b1ccb03f5fce
balabolchik <obordulyak@gmail.com>
2020-10-11 22:56:29 +0300

    1

---
+++ js/index.js
@@ -0,0 +1,41 @@
+// let name = "Oleg";
+
+// if (name==="Oleg"){
+//     console.log(`Hello, Boss!!!`);
+// } else if(name==="Nastya"){
+//     console.log(`I love you, ${name}!!!`);
+// } else if(name==="Vitia"){
+//     console.log(`Hello, ${name}!!!`)
+// } else {
+//     console.log("Hello, stranger!!!");
+// };
+
+// let i = 2;
+// while (i<=100){
+//     i=i*i/(0.5*i);
+//     console.log(i);
+// };
+
+
+
+// for (let sheepCounted=0;sheepCounted<=10;sheepCounted++){
+//     console.log(`Посчитано овец: ${sheepCounted}!`);
+//     sheepCounted++;
+// };
+// console.log("Xpppppppppp-Xppp");
+
+// let animals = ["lion", "flamingo", "white bear", "boa"];
+
+// for (let i = 0; i<animals.length;i++){
+//     console.log(`In this zoo is ${animals[i]}.`);
+// };
+
+// let name = "Anastasya";
+
+// for (let i=0; i<name.length;i++){
+//     console.log(`In my name is a latter "${name[i]}"`);
+// };
+
+for (let i = 1; i<=10000000; i=i*3){
+    console.log(i);
+};
